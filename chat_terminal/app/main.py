0552df003209f17076079b7d4048be3e668cad70
bbonassi <bbonassi@lliege.com.br>
2025-07-25 15:58:20 -0300

    estrutura basica testando

---
+++ chat_terminal/app/main.py
@@ -0,0 +1,16 @@
+def main():
+    while True:
+        command = input(">> ")
+
+        if command == "exit":
+            break
+
+        #tabelas
+        #CRUD
+        #interpretar comandos
+
+class Teste():
+    
+    
+    #pensar sobre interpretar classes como models
+    nome = ''
